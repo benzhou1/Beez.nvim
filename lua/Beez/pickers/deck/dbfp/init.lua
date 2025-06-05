5ce014b57e63c04a48b76c4cd41258a23c383a13
Ben Zhou <benzhou29@gmail.com>
2025-06-05 08:52:20 -0500

    refactor(pickers.dbfp): Handle lazy loading

--- lua/Beez/pickers/deck/dbfp/init.lua
+++ lua/Beez/pickers/deck/dbfp/init.lua
@@ -1,6 +1,28 @@
-local M = require("Beez.pickers.deck.dbfp.pickers")
+local sources = require("Beez.pickers.deck.dbfp.sources")
+local M = {}
 
-M.actions = require("Beez.pickers.deck.dbfp.actions")
-M.sources = require("Beez.pickers.deck.dbfp.actions")
+--- Deck for dbfp connections
+---@param opts table?
+function M.connections(opts)
+  opts = vim.tbl_deep_extend("keep", opts or {}, { prompt = false })
+  local source, specifier = sources.connections(opts)
+  require("deck").start(source, specifier)
+end
+
+--- Deck for dbfp queries
+---@param opts table?
+function M.queries(opts)
+  opts = opts or {}
+  local source, specifier = sources.queries(opts)
+  require("deck").start(source, specifier)
+end
+
+--- Deck for listing query files
+---@param opts {connection?: string}?
+function M.queryfiles(opts)
+  opts = opts or {}
+  local source, specifier = sources.queryfiles(opts)
+  require("deck").start(source, specifier)
+end
 
 return M
