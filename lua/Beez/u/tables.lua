02e05323bb682985be460768f65ee08326a35879
Ben Zhou <benzhou29@gmail.com>
2025-06-15 15:58:55 -0500

    feat(u.tables): Added new util function for counting the size of a table

--- lua/Beez/u/tables.lua
+++ lua/Beez/u/tables.lua
@@ -86,4 +86,15 @@ function M.remove(t, rv)
   end
 end
 
+--- Count length of table, handles dictionary as well
+---@param t table
+---@return integer
+function M.len(t)
+  local count = 0
+  for _, _ in pairs(t) do
+    count = count + 1
+  end
+  return count
+end
+
 return M
