828d434efab291cd2adda912c38e70621fdbb660
Ben Zhou <benzhou29@gmail.com>
2025-08-25 08:53:36 -0500

    feat(u.keymaps): Allow for picking out keys from keymap set

--- lua/Beez/u/keymaps.lua
+++ lua/Beez/u/keymaps.lua
@@ -1,22 +1,32 @@
 local M = {}
 
 --- Set keymaps like lazy keys spec
+---@param opts? {pick: string[]}
 ---@param keymaps table
-function M.set(keymaps)
+function M.set(keymaps, opts)
+  local u = require("Beez.u")
+  opts = opts or {}
   for _, km in ipairs(keymaps) do
     local key = km[1]
     local action = km[2]
     local mode = km.mode or "n"
-    if action == false then
-      vim.keymap.del(mode, key)
-    else
-      vim.keymap.set(mode, key, action, {
-        desc = km.desc,
-        nowait = km.nowait,
-        expr = km.expr,
-        remap = km.remap,
-        buffer = km.buffer,
-      })
+
+    local valid = true
+    if opts.pick ~= nil and not u.tables.contains(opts.pick, key:lower()) then
+      valid = false
+    end
+    if valid then
+      if action == false then
+        vim.keymap.del(mode, key)
+      else
+        vim.keymap.set(mode, key, action, {
+          desc = km.desc,
+          nowait = km.nowait,
+          expr = km.expr,
+          remap = km.remap,
+          buffer = km.buffer,
+        })
+      end
     end
   end
 end
