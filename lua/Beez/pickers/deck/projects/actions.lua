124995ba1390fc9f0653d43f5961b11a76876043
Ben Zhou <benzhou29@gmail.com>
2025-12-18 16:23:58 -0600

    feat(pickers.projects): Added action to run an external command through neovide

--- lua/Beez/pickers/deck/projects/actions.lua
+++ lua/Beez/pickers/deck/projects/actions.lua
@@ -1,6 +1,31 @@
 local M = {}
 local cmds = require("Beez.cmds")
 
+M.run_cmd_external = {}
+--- Deck action to run a command in a neovide terminal window
+---@param opts? table
+---@return deck.Action[]
+function M.run_cmd_external.action(opts)
+  opts = opts or {}
+  return {
+    require("deck").alias_action("default", M.run_cmd_external.name),
+    {
+      name = M.run_cmd_external.name,
+      execute = function(ctx)
+        local item = ctx.get_action_items()[1]
+        cmds.neovide.run_cmd(item.data.cmd)
+        ctx:hide()
+        if opts.quit then
+          vim.schedule(function()
+            vim.cmd("q")
+          end)
+        end
+      end,
+    },
+  }
+end
+M.run_cmd_external.name = "scripts.run_cmd_external"
+
 M.open_neohub = {}
 --- Deck action to open project with neohub
 ---@param opts? table
