2dc3f5ef9e9d5f4c26789c31f4e6c1c7f5485036
Ben Zhou <benzhou29@gmail.com>
2025-12-18 16:23:58 -0600

    feat(u.os): Added util functions for walking and copying a folder

--- lua/Beez/u/os.lua
+++ lua/Beez/u/os.lua
@@ -1,3 +1,4 @@
+local uv = vim.uv or vim.loop
 local M = { cache = {} }
 
 --- Is os windows
@@ -86,6 +87,58 @@ function M.mtime(path)
     return mtime
   end
   return 0
+end
+
+--- Walk a directory path recursively similar to python walk
+---@param dir_path string
+---@return fun(): string, string[], string[]
+function M.walk(dir_path)
+  return coroutine.wrap(function()
+    local stack = { dir_path }
+
+    while #stack > 0 do
+      local current = table.remove(stack)
+      local dirs, files = {}, {}
+      for name, type in vim.fs.dir(current) do
+        local path = vim.fs.joinpath(current, name)
+        if type == "directory" then
+          table.insert(stack, path)
+          table.insert(dirs, name)
+        else
+          table.insert(files, name)
+        end
+      end
+      coroutine.yield(current, dirs, files)
+    end
+  end)
+end
+
+--- Copies a directory
+---@param src string
+---@param dest string
+---@param opts? {chmod_mode: integer}
+function M.copy_dir(src, dest, opts)
+  opts = opts or {}
+
+  uv.fs_mkdir(dest, 493) -- 0755
+  for root, dirs, files in M.walk(src) do
+    local relative_path = vim.fs.relpath(src, root)
+    for _, d in ipairs(dirs) do
+      local src_dir = vim.fs.joinpath(root, d)
+      local dest_dir = vim.fs.joinpath(dest, relative_path, d)
+      local stat = uv.fs_stat(src_dir)
+      uv.fs_mkdir(dest_dir, stat.mode)
+    end
+    for _, f in ipairs(files) do
+      local src_file = vim.fs.joinpath(root, f)
+      local dest_file = vim.fs.joinpath(dest, relative_path, f)
+      uv.fs_copyfile(src_file, dest_file)
+
+      if opts.chmod_mode then
+        uv.fs_chmod(dest_file, opts.chmod_mode)
+      end
+    end
+  end
 end
 
 return M
