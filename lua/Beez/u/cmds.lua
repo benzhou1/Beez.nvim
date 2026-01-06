db6537b9746d22fef7310ceba09ade791969710e
Ben Zhou <benzhou29@gmail.com>
2026-01-05 21:19:49 -0600

    feat(u.cmds): Support detach

--- lua/Beez/u/cmds.lua
+++ lua/Beez/u/cmds.lua
@@ -3,14 +3,20 @@ local M = {}
 --- Runs a command. Async if cb is provided.
 ---@param cmd string[]
 ---@param cb? fun(err: string|nil, stdout: string|nil)
----@param opts? {ignore_err?: string[]}
+---@param opts? {ignore_err?: string[], env?: table<string, string>, cwd?: string, detach?: boolean}
 function M.run(cmd, cb, opts)
   opts = opts or {}
+  local system_opts = {
+    text = true,
+    env = opts.env,
+    cwd = opts.cwd,
+    detach = opts.detach,
+  }
   if cb == nil then
-    return vim.system(cmd, { text = true }):wait()
+    return vim.system(cmd, system_opts):wait()
   end
 
-  vim.system(cmd, { text = true }, function(obj)
+  vim.system(cmd, system_opts, function(obj)
     if obj.code ~= 0 then
       if opts.ignore_err ~= nil then
         for _, ie in ipairs(opts.ignore_err) do
@@ -19,7 +25,12 @@ function M.run(cmd, cb, opts)
           end
         end
       end
-      vim.notify("Error running '" .. table.concat(cmd, " ") .. "': " .. obj.stderr, vim.log.levels.WARN)
+      vim.schedule(function()
+        vim.notify(
+          "Error running '" .. table.concat(cmd, " ") .. "': " .. obj.stderr,
+          vim.log.levels.WARN
+        )
+      end)
       return cb(obj.stderr, nil)
     end
     cb(nil, obj.stdout)
