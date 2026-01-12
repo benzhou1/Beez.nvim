4e29a476a501d1ac0a124f52f43146c5980e04c7
Ben Zhou <benzhou29@gmail.com>
2026-01-12 10:22:02 -0600

    feat(pickers.lsp): Fix overseer peek action to fallback to opening in buffer if overseer is not installed

--- lua/Beez/pickers/deck/lsp/actions.lua
+++ lua/Beez/pickers/deck/lsp/actions.lua
@@ -9,9 +9,16 @@ function M.peek()
       name = "overlook.peek",
       execute = function(ctx)
         local item = ctx.get_action_items()[1]
+
+        local ok, overlook_ui = pcall(require, "overlook.ui")
+        if not ok then
+          vim.cmd("e " .. item.data.filename)
+          return
+        end
+
         ctx.hide()
         vim.schedule(function()
-          require("overlook.ui").create_popup({
+          overlook_ui.create_popup({
             target_bufnr = item.data.target_bufnr,
             lnum = item.data.lnum,
             col = item.data.col,
